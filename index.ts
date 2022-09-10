80df181f7f1c5108a39510fa87f831c2d4d8414b
MichalRsa <michalrosa17@gmail.com>
2022-09-10 11:07:19 +0200

    Add startServer function

---
+++ index.ts
@@ -0,0 +1,4 @@
+import AppDataSource from './config/database';
+import startServer from './server';
+
+startServer(AppDataSource);
--- server.ts
+++ server.ts
@@ -1,28 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import AppDataSource from './config/database';
+import { DataSource } from 'typeorm';
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
+const startServer = async (DB: DataSource) => {
+  await DB.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((err: any) => {
+      console.error('Error during Data Source initialization', err);
+    });
 
-    const path = require('path');
+  const path = require('path');
 
-    dotenv.config();
+  dotenv.config();
 
-    let port =
-      typeof process.env.PORT === 'string' && parseFloat(process.env.PORT);
+  let port = Number(process.env.POSTGRES_PORT);
 
-    const app = express();
+  const app = express();
 
-    if (!port) {
-      port = 3000;
-    }
+  if (!port) {
+    port = 3000;
+  }
 
-    app.get('/', (req, res) => res.send('ok'));
+  app.get('/', (req, res) => res.send('ok'));
 
-    app.listen(port, () => console.log(`Server is running on port ${port}`));
-  })
-  .catch((err: any) => {
-    console.error('Error during Data Source initialization', err);
-  });
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+};
+
+export default startServer;
