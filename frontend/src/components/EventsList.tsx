6867693f8871965f9d78b7f62d8c892958de3c4d
MichalRsa <michalrosa17@gmail.com>
2022-09-14 16:21:13 +0200

    Add list view for zero events

--- frontend/src/components/EventsList.tsx
+++ frontend/src/components/EventsList.tsx
@@ -4,10 +4,13 @@ import { EventData } from '../views/Main';
 const EventsList = ({ events }: { events: EventData[] }) => {
   return (
     <div className='max-full flex flex-wrap justify-around'>
-      {!!events.length &&
+      {!!events.length ? (
         events.map((eventItem) => (
           <Event key={eventItem.id} eventItem={eventItem} />
-        ))}
+        ))
+      ) : (
+        <p className='py-8'>Add your first event!</p>
+      )}
     </div>
   );
 };
