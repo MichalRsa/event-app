f257107da024d2693ab50b90e3c4684b2e645b49
MichalRsa <michalrosa17@gmail.com>
2022-09-15 21:15:59 +0200

    Remove any anotation

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -1,11 +1,23 @@
 import { useField } from 'formik';
 
-const TextInput = ({ label, ...props }: any) => {
+type Label = 'First Name' | 'Last Name' | 'Email Address' | 'Date';
+type Name = 'firstName' | 'lastName' | 'email' | 'date';
+type Type = 'datetime-local' | 'text' | 'email';
+
+interface TextInputProps {
+  label: Label;
+  name: Name;
+  id: Name;
+  placeholder?: string;
+  type: Type;
+}
+
+const TextInput = (props: TextInputProps) => {
   const [field, meta] = useField(props);
   return (
     <div className='flex flex-col my-4'>
       <label className=' py-1' htmlFor={props.id || props.name}>
-        {label}
+        {props.label}
       </label>
       <input className='p-2 text-xs' {...field} {...props} />
       {meta.touched && meta.error ? (
