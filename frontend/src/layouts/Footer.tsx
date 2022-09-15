2a8f35ad89b4b1204c9aac3f7a134ab9f53ec2b0
MichalRsa <michalrosa17@gmail.com>
2022-09-15 16:04:06 +0200

    Add link to repository in Footer

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -2,7 +2,11 @@ const Footer = () => {
   return (
     <footer className='py-4 bg-slate-900'>
       <div className='max-w-screen-lg mx-auto'>
-        <p className='text-sky-100'>Footer</p>
+        <p className='text-sky-100'>
+          <a href='https://github.com/MichalRsa/event-app' target='_blank'>
+            Checkout Github repository
+          </a>
+        </p>
       </div>
     </footer>
   );
