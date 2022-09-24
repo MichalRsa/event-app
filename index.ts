ed72c2e2b059c0ebfb79b987cb52e5c5e00b7b64
MichalRsa <michalrosa17@gmail.com>
2022-09-24 19:32:03 +0200

    Move app.listen outside of startServer function

--- index.ts
+++ index.ts
@@ -2,4 +2,6 @@ import AppDataSource from './config/database';
 import eventController from './controller/eventController';
 import startServer from './server';
 
-startServer(AppDataSource, [eventController(AppDataSource)]);
+startServer(AppDataSource, [eventController(AppDataSource)]).then((app) =>
+  app.listen(3000, () => console.log(`Server is running on port ${3000}`))
+);
