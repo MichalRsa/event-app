6dcbae8bcf98a7f5f1540aaa081b2d3ea922b658
Michał Rosa <michalrosa@MacBook-Pro-Micha.local>
2022-11-06 15:58:35 +0100

    Add User entity

---
+++ entity/user.entity.ts
@@ -0,0 +1,21 @@
+import { IsEmail } from 'class-validator';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  password: string;
+}
