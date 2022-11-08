0342f30a479dde4e8fad54e74b9b006e9c7600bc
Michał Rosa <michalrosa17@gmail.com>
2022-11-08 16:12:40 +0100

    Fix jwt config var name

--- service/authenticationService.ts
+++ service/authenticationService.ts
@@ -12,11 +12,11 @@ import { User } from '../entity/user.entity';
 import SomethindWentWrongException from '../exceptions/SomethingWentWrongException';
 import { UserRepository } from '../repository/UserRepository';
 
-const dbConfig: any = config.get('jwt');
+const jwtConfig: any = config.get('jwt');
 
 const createToken = (user: User): TokenData => {
   const expiresIn = 60 * 60; // an hour
-  const secret = dbConfig.secret;
+  const secret = jwtConfig.secret;
   const dataStoredInToken: DataStoredInToken = {
     id: user.id.toString(),
   };
